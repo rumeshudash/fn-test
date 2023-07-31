7cc426f70e76308b5262cfe4aa46b60479c87ea4
bibas07 <koeralabibas@gmail.com>
2023-07-31 12:15:11 +0545

    added signup flows

---
+++ src/helpers/signup.helper.ts
@@ -0,0 +1,43 @@
+import { expect } from "@playwright/test";
+import { BaseHelper } from "./BaseHelper/base.helper";
+import { uuidV4 } from "@/utils/common.utils";
+
+export class SignupHelper extends BaseHelper {
+  private static SIGNUP_DOM_SELECTOR = "//form/parent::div";
+
+  public async init() {
+    await this.navigateTo("SIGNUP");
+  }
+
+  public static genRandomEmail() {
+    return `test-${uuidV4()}@gmail.com`;
+  }
+  public async nextPage() {
+    await this.click({ text: "Next" });
+    await this._page.waitForTimeout(2000);
+  }
+
+  public async errorMessage() {
+    return this._page
+      .locator('//span[contains(@class, "label-text")]')
+      .textContent();
+  }
+
+  public async isTermsChecked() {
+    return this._page.locator("//input[@type='checkbox']");
+  }
+  public async clickPolicy() {
+    this._page.locator("//input[@type='checkbox']").click();
+  }
+
+  public async fillSignup(data: SignupDetailsInput) {
+    const helper = this.locate(SignupHelper.SIGNUP_DOM_SELECTOR);
+
+    await helper.fillInput(data.name, { name: "name" });
+    await helper.fillInput(data.email, { name: "username" });
+    await helper.fillInput(data.password, { name: "password" });
+    await helper.fillInput(data.confirm_password, {
+      name: "confirmPassword",
+    });
+  }
+}
--- src/tests/Expenses/raise_expense_without_poc.spec.ts
+++ src/tests/Expenses/raise_expense_without_poc.spec.ts
@@ -4,39 +4,39 @@ import { ExpenseHelper } from "@/helpers/expense.helper";
 const { expect, describe } = PROCESS_TEST;
 
 describe("Raise Expense", () => {
-    PROCESS_TEST("without poc", async ({ page }) => {
-        const expense = new ExpenseHelper(page);
+  PROCESS_TEST("without poc", async ({ page }) => {
+    const expense = new ExpenseHelper(page);
 
-        await expense.init();
+    await expense.init();
 
-        await expense.nextPage();
-        await expense.fillExpenses([
-            {
-                to_nth: 1,
-                from_nth: 1,
-                amount: 1000,
-                taxable_amount: 900,
-            },
-        ]);
-    });
+    await expense.nextPage();
+    await expense.fillExpenses([
+      {
+        to_nth: 1,
+        from_nth: 1,
+        amount: 1000,
+        taxable_amount: 900,
+      },
+    ]);
+  });
 
-    PROCESS_TEST("with poc", async ({ page }) => {
-        const expense = new ExpenseHelper(page);
+  PROCESS_TEST("with poc", async ({ page }) => {
+    const expense = new ExpenseHelper(page);
 
-        await expense.init();
+    await expense.init();
 
-        await expense.nextPage();
-        await expense.fillExpenses([
-            {
-                to_nth: 1,
-                from_nth: 1,
-                amount: 10000,
-                taxable_amount: 9000,
-                department: "Hr",
-                expense_head: "Rent",
-                poc: "Abhishek",
-                pay_to: "Employee",
-            },
-        ]);
-    });
+    await expense.nextPage();
+    await expense.fillExpenses([
+      {
+        to_nth: 1,
+        from_nth: 1,
+        amount: 10000,
+        taxable_amount: 9000,
+        department: "Hr",
+        expense_head: "Rent",
+        poc: "Abhishek",
+        pay_to: "Employee",
+      },
+    ]);
+  });
 });
