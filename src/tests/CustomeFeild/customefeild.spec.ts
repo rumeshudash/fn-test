74b35e8ba4326f15ab5d466692ad72b42df19dab
asujan205 <asujan205@gmail.com>
2023-09-08 13:22:18 +0545

    #Add Expense Without Type Feilds

--- src/tests/CustomeFeild/customefeild.spec.ts
+++ src/tests/CustomeFeild/customefeild.spec.ts
@@ -53,4 +53,61 @@ test.describe('CustomeFeild', () => {
         await customefeild.clickButton('Add New');
         await expect(page.getByText('Add Expense Custom Field')).toHaveCount(1);
     });
+    test('Add Expense With Empty Feilds', async ({ page }) => {
+        const signin = new SignInHelper(page);
+        await signin.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signin.checkDashboard({
+            username: username,
+            password: password,
+        });
+
+        const customefeild = new CustofeildHelper(page);
+        await customefeild.init();
+        await customefeild.clickExpenseTab();
+        await customefeild.clickButton('Add New');
+
+        await customefeild.clickButton('Save');
+
+        await expect(page.getByRole('button', { name: 'Save' })).toHaveCount(1);
+    });
+
+    test('Add Expense Without Name Feilds', async ({ page }) => {
+        const signin = new SignInHelper(page);
+        await signin.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signin.checkDashboard({
+            username: username,
+            password: password,
+        });
+
+        const customefeild = new CustofeildHelper(page);
+        await customefeild.init();
+
+        await customefeild.AddExpenseCustomeFeild('', 'Text', 1);
+
+        expect(await customefeild.errorMessage()).toBe(
+            'Field Name is required'
+        );
+    });
+    test('Add Expense Without Type Feilds', async ({ page }) => {
+        const signin = new SignInHelper(page);
+        await signin.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signin.checkDashboard({
+            username: username,
+            password: password,
+        });
+
+        const customefeild = new CustofeildHelper(page);
+        await customefeild.init();
+        await customefeild.AddExpenseCustomeFeild('Test1', '', 1);
+
+        expect(await customefeild.errorMessage()).toBe(
+            'Field Type is required'
+        );
+    });
 });
