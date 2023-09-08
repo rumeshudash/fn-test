cb097ba1a6ee18a7536e30d9bc3406c226093bf3
asujan205 <asujan205@gmail.com>
2023-09-08 16:01:55 +0545

    #expense type with date feild

--- src/tests/CustomeFeild/customefeild.spec.ts
+++ src/tests/CustomeFeild/customefeild.spec.ts
@@ -183,4 +183,19 @@ test.describe('CustomeFeild', () => {
 
         await expect(page.getByText('TextArea1')).toHaveCount(1);
     });
+    test('Add Expense With Date type', async ({ page }) => {
+        const signin = new SignInHelper(page);
+        await signin.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signin.checkDashboard({
+            username: username,
+            password: password,
+        });
+
+        const customefeild = new CustofeildHelper(page);
+        await customefeild.init();
+
+        await customefeild.AddExpenseWithDateType('Date1', 'Date', 1);
+    });
 });
