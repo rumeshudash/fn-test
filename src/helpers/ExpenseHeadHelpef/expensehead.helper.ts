cce4bd6c81406a99b3de2dd15ed668b6b836470d
asujan205 <asujan205@gmail.com>
2023-09-09 16:05:31 +0545

    #withou-name-feild

--- src/helpers/ExpenseHeadHelpef/expensehead.helper.ts
+++ src/helpers/ExpenseHeadHelpef/expensehead.helper.ts
@@ -5,4 +5,35 @@ export class ExpenseHeadHelper extends BaseHelper {
     public async init() {
         await this.navigateTo('EXPENSE_HEADS');
     }
+
+    public async AddExpenseHead(
+        name: string,
+        parent?: string,
+        manager?: string,
+        abc?: string
+    ) {
+        await this.clickButton('Add Expense Head');
+        await this.fillText(name, {
+            name: 'name',
+        });
+        // if(parent){
+        //     await this.selectOption({
+        //         option: parent,
+        //         hasText: 'Select Parent',
+        //     });
+        // }
+        // if(manager){
+        //     await this.selectOption({
+        //         option: manager,
+        //         hasText: 'Select Manager',
+        //     });
+        // }
+        // if(abc){
+        //     await this.selectOption({
+        //         option: abc,
+        //         hasText: 'Select ABC',
+        //     });
+        // }
+        await this.click({ role: 'button', name: 'save' });
+    }
 }
--- src/tests/ExpenseHead/expensehead.creation.spec.ts
+++ src/tests/ExpenseHead/expensehead.creation.spec.ts
@@ -19,4 +19,35 @@ test.describe('Expense Head', () => {
 
         await expect(page.getByText('Expense Heads')).toHaveCount(2);
     });
+    test('Click on Add Expense Head', async ({ page }) => {
+        const signin = new SignInHelper(page);
+        await signin.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signin.checkDashboard({
+            username: username,
+            password: password,
+        });
+
+        const expenseHead = new ExpenseHeadHelper(page);
+        await expenseHead.init();
+
+        await expenseHead.clickButton('Add Expense Head');
+        await expect(page.getByText('Add Expense Head')).toHaveCount(2);
+    });
+    test('Create Expense Head empty Name feild', async ({ page }) => {
+        const signin = new SignInHelper(page);
+        await signin.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signin.checkDashboard({
+            username: username,
+            password: password,
+        });
+
+        const expenseHead = new ExpenseHeadHelper(page);
+        await expenseHead.init();
+        await expenseHead.AddExpenseHead('');
+        await expect(await expenseHead.errorMessage()).toBe('Name is required');
+    });
 });
