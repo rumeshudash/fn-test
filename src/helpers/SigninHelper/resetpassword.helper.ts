b0c092ddb999abb0fb1b59d1b53d14fba838e106
asujan205 <asujan205@gmail.com>
2023-09-09 14:17:41 +0545

    #reset-password

--- src/helpers/SigninHelper/resetpassword.helper.ts
+++ src/helpers/SigninHelper/resetpassword.helper.ts
@@ -12,6 +12,57 @@ export class ResetPasswordHelper extends BaseHelper {
 
         await this._page.waitForTimeout(1000);
 
-        await expect(this._page.getByText('Change Password')).toHaveCount(1);
+        // await expect(this._page.getByText('Change Password')).toHaveCount(1);
+        // await this._page.waitForTimeout(1000);
+    }
+    public async resetPassword(
+        password: string,
+        newpassword: string,
+        confirm: string
+    ) {
+        this._page.getByRole('button', { name: 'Actions' }).click();
+        await this._page.waitForTimeout(1000);
+        this._page.getByRole('menuitem', { name: 'Change Password' }).click();
+
+        await this._page.waitForTimeout(1000);
+
+        await this.fillText(password, {
+            id: 'old_password',
+        });
+        await this.fillText(newpassword, {
+            id: 'password',
+        });
+        await this.fillText(confirm, {
+            id: 'confirm_password',
+        });
+        await this.click({ role: 'button', name: 'Save' });
+    }
+
+    public async resetPasswordsameoldAndNewPassword(
+        password: string,
+        newpassword: string
+    ) {
+        this._page.getByRole('button', { name: 'Actions' }).click();
+        await this._page.waitForTimeout(1000);
+        this._page.getByRole('menuitem', { name: 'Change Password' }).click();
+
+        await this._page.waitForTimeout(1000);
+
+        await this.fillText(password, {
+            id: 'old_password',
+        });
+        await this.fillText(newpassword, {
+            id: 'password',
+        });
+
+        this._page.waitForTimeout(1000);
+    }
+
+    public async errorSameMessage() {
+        let message = await this._page
+            .locator('//span[contains(@class, "label-text")]')
+            .textContent();
+
+        console.log(message);
     }
 }
--- src/tests/Signin/resetpassword.spec.ts
+++ src/tests/Signin/resetpassword.spec.ts
@@ -18,4 +18,72 @@ test.describe('Reset Password', () => {
 
         await expect(page.getByText('Change Password')).toHaveCount(1);
     });
+    test('Reset Password with empty password', async ({ page }) => {
+        const signIn = new SignInHelper(page);
+        await signIn.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signIn.checkDashboard({
+            username: username,
+            password: password,
+        });
+        const resetPassword = new ResetPasswordHelper(page);
+        await resetPassword.init();
+
+        await resetPassword.resetPassword('', '1234567', '1234567');
+        expect(await signIn.errorMessage()).toBe('Old Password is required');
+    });
+    test('Reset Password with Incorrect  new password', async ({ page }) => {
+        const signIn = new SignInHelper(page);
+        await signIn.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signIn.checkDashboard({
+            username: username,
+            password: password,
+        });
+        const resetPassword = new ResetPasswordHelper(page);
+        await resetPassword.init();
+
+        await resetPassword.resetPassword('1234544', '12', '12345678');
+
+        expect(await signIn.errorMessage()).toBe(
+            'Confirm New Password does not match'
+        );
+    });
+    test('Reset Password with Without confirm password', async ({ page }) => {
+        const signIn = new SignInHelper(page);
+        await signIn.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signIn.checkDashboard({
+            username: username,
+            password: password,
+        });
+        const resetPassword = new ResetPasswordHelper(page);
+        await resetPassword.init();
+        await resetPassword.resetPassword('1234544', '121234544', '');
+
+        expect(await signIn.errorMessage()).toBe(
+            'Confirm New Password is required'
+        );
+    });
+    test('Reset Password with same old and  new password', async ({ page }) => {
+        const signIn = new SignInHelper(page);
+        await signIn.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signIn.checkDashboard({
+            username: username,
+            password: password,
+        });
+        const resetPassword = new ResetPasswordHelper(page);
+        await resetPassword.init();
+        await resetPassword.resetPassword('1234544', '1234544', '1234544');
+
+        expect(await signIn.errorMessage()).toBe(
+            "Your old password can't be same as your New password"
+        );
+    });
+    test('Validate The length of new password', async ({ page }) => {});
 });
