22eee65685ac7f88b4f5af0faf30ef3998d2cb17
asujan205 <asujan205@gmail.com>
2023-09-09 15:48:13 +0545

    #check-expense-head-page

--- src/tests/ExpenseHead/expensehead.creation.spec.ts
+++ src/tests/ExpenseHead/expensehead.creation.spec.ts
@@ -1,1 +1,22 @@
 import { ExpenseHeadHelper } from '@/helpers/ExpenseHeadHelpef/expensehead.helper';
+import { SignInHelper } from '@/helpers/SigninHelper/signIn.helper';
+
+import { test, expect } from '@playwright/test';
+
+test.describe('Expense Head', () => {
+    test('Expense Head page is open', async ({ page }) => {
+        const signin = new SignInHelper(page);
+        await signin.init();
+        const username = 'newtestauto@company.com';
+        const password = '123456';
+        await signin.checkDashboard({
+            username: username,
+            password: password,
+        });
+
+        const expenseHead = new ExpenseHeadHelper(page);
+        await expenseHead.init();
+
+        await expect(page.getByText('Expense Heads')).toHaveCount(2);
+    });
+});
