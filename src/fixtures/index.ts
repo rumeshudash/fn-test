b4b17a6d25b66d28859cbf617e3d72d96a2d722a
Rumesh Udash <rumesh.udas@gmail.com>
2023-09-11 21:14:01 +0545

    Add env for frontend url & username/password for test account

--- src/fixtures/index.ts
+++ src/fixtures/index.ts
@@ -17,12 +17,17 @@ export const PROCESS_TEST = test.extend<{ login: void }>({
             await page.waitForLoadState('networkidle');
 
             if (await helper.isVisible({ id: 'username' })) {
-                await helper.fillInput('newtestauto@company.com', {
-                    name: 'username',
-                });
+                await helper.fillInput(
+                    process.env.TEST_USER || 'newtestauto@company.com',
+                    {
+                        name: 'username',
+                    }
+                );
                 await helper.click({ role: 'button', name: 'Next →' });
 
-                await helper.fillInput('123456', { name: 'password' });
+                await helper.fillInput(process.env.TEST_PASS || '123456', {
+                    name: 'password',
+                });
                 await helper.click({ role: 'button', name: 'Submit' });
 
                 await page.getByRole('dialog').waitFor({
