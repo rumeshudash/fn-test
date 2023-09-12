61632806294e12e88d604e1a1e4367a4c32038e1
asujan205 <asujan205@gmail.com>
2023-09-12 10:22:23 +0545

    #grades.helper

--- src/helpers/GradesHelper/grade.helper.ts
+++ src/helpers/GradesHelper/grade.helper.ts
@@ -1,10 +1,18 @@
 import { generateRandomNumber } from '@/utils/common.utils';
 import { BaseHelper } from '../BaseHelper/base.helper';
 import { expect } from '@playwright/test';
+import { ListingHelper } from '../BaseHelper/listing.helper';
 
 export class GradesHelper extends BaseHelper {
     private static GRADES_DOM_SELECTOR =
         "(//div[contains(@class,'flex-1 h-full')])[1]";
+
+    public listin: ListingHelper;
+
+    constructor(page: any) {
+        super(page);
+        this.listin = new ListingHelper(page);
+    }
 
     public async init() {
         await this.navigateTo('GRADES');
@@ -46,13 +54,8 @@ export class GradesHelper extends BaseHelper {
         await this.click({ role: 'button', name: 'save' });
     }
     public async ActiveToInactive(name: string) {
-        async function performAction(element: any) {
-            await element.click();
-        }
-
-        const btnlocator = '//button';
-
-        await this.FindrowAndperformAction(name, 3, btnlocator, performAction);
+        const row = await this.listin.findRowInTable(name, 'NAME');
+        await this.listin.clickButtonInTable(row, 'STATUS');
     }
 
     public async EditGrdaes(name: string, newname: string, priority: number) {
