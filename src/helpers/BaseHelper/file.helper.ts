48a21b065da77d29f0a1a0179d471597c59f08b6
Rumesh Udash <rumesh.udas@gmail.com>
2023-09-19 10:41:51 +0545

    Fix - file helper

--- src/helpers/BaseHelper/file.helper.ts
+++ src/helpers/BaseHelper/file.helper.ts
@@ -42,10 +42,10 @@ export class FileHelper extends BaseHelper {
         await dialogLocator
             .locator("//input[@type='file']")
             .setInputFiles(`images/pan-card.jpg`);
+        const response = await this._page.waitForResponse('**/upload-files');
         await this._page.waitForLoadState('networkidle');
         await this._page.waitForTimeout(2000);
 
-        const response = await this._page.waitForResponse('**/upload-files');
         expect(response.status()).toBe(201);
     }
 }
