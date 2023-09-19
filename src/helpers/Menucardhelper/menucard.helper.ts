8c8f7dc829b8e92789fd2abdefb7cf71238bb7a7
asujan205 <asujan205@gmail.com>
2023-09-19 11:59:35 +0545

    #menu-card

--- src/helpers/Menucardhelper/menucard.helper.ts
+++ src/helpers/Menucardhelper/menucard.helper.ts
@@ -1,5 +1,45 @@
 import chalk from 'chalk';
 import { PageHelper } from '../BaseHelper/page.helper';
+import { expect } from '@playwright/test';
+
+const mainMenuArray = [
+    'Dashboard',
+    'Expenses',
+    'Advances',
+    'Parties',
+    'HR',
+    'Work Flows',
+    'Transactions',
+    'Configurations',
+    'Reports',
+    'Switch Mode',
+    'Settings',
+    'Logout',
+];
+const submenuObject = {
+    Dashboard: ['AP Dashboard', 'Payment Dashboard'],
+
+    Parties: ['My Bussiness', 'My Vendors', 'Vendor Invitations'],
+
+    HR: ['Employees', 'Departments', 'Designations', 'Grades'],
+
+    'Work Flows': [
+        'Expense Approval',
+        'Advance Approval',
+        'Approval Delegations',
+        'Expense Aprroval Limits',
+        'Advance Approval Limits',
+    ],
+    Transactions: [
+        'All Payments',
+        'Cash Transactions',
+        'Online Bank Transfer',
+        'Cheque Payments',
+        'Bank Accounts',
+        'Gstr2b',
+    ],
+    Settings: ['My Profile', 'User Management'],
+};
 
 export class MenucardHelper extends PageHelper {
     public async init() {
@@ -7,17 +47,114 @@ export class MenucardHelper extends PageHelper {
     }
 
     public async openAndCloseMenuCard() {
+        await this._page.waitForLoadState('networkidle');
         await this._page
             .locator(`//div[contains(@class,'hamburger_button')]`)
             .click();
     }
 
-    public async getSideBarItems() {
-        const locator = await this.locate(
+    public async checkSideBarItems() {
+        await this.openAndCloseMenuCard();
+        const itemsText = await this._page
+            .locator(`//div[contains(@class,'sidebar-items')]`)
+            .innerText();
+
+        const itemsArray = itemsText.split('\n');
+
+        const filteredItems = itemsArray.filter((item) => item.trim() !== '');
+
+        const excludedItems = [
+            'grid_view',
+            'arrow_drop_down',
+            'NT',
+            'New Test Auto',
+            'newtestauto@company.com',
+            'arrow_right',
+        ];
+
+        const filteredAndCleanedItems = filteredItems.filter((item) => {
+            return !excludedItems.includes(item);
+        });
+
+        console.log('filteredAndCleanedItems:', filteredAndCleanedItems);
+        expect(filteredAndCleanedItems).toEqual(mainMenuArray);
+    }
+
+    public async clickOnSideBarMenu(menuName: string) {
+        await this._page.waitForTimeout(1000);
+        await this._page
+            .locator(`//div[contains(@class,'sidebar-items')]`)
+            .getByText(menuName, { exact: true })
+            .click();
+    }
+
+    public async getSidebarSubMenu(menuName: string) {
+        await this.clickOnSideBarMenu(menuName);
+        const submenu = await this._page
+            .locator(`(//div[contains(@class,'menus hover')]//a)`)
+            .innerText();
+
+        console.log('sub menu', submenu);
+    }
+    public async checkSpaceInMenu(menuName: string) {
+        const locator = await this._page
+            .locator(
+                `//div[contains(@class,'sidebar-items')]//p[contains(@class,'sidebar-item-title')]`
+            )
+            .filter({
+                hasText: menuName,
+            });
+
+        expect(
+            locator.locator(`//ancestor::div[contains(@class,'submenu mt-3')]`)
+        ).toBeTruthy();
+    }
+
+    public async checkSubMenuItems(menuName: string) {
+        const locator = await this._page
+            .locator(
+                `//div[contains(@class,'sidebar-items')]//p[contains(@class,'sidebar-item-title')]`
+            )
+            .filter({
+                hasText: menuName,
+            });
+        const getAncestor = await locator.locator(
+            `//ancestor::div[contains(@class,'submenu')]`
+        );
+
+        const menus = await getAncestor
+            .locator(`//div[contains(@class,'menus')]`)
+            .textContent(); // Use textContent instead of innerText
+
+        const newMenu = menus.split('\n');
+
+        const filteredItems = newMenu.filter((item) => item.trim() !== '');
+
+        expect(filteredItems).toEqual(submenuObject[menuName]);
+    }
+
+    public async checkSpaceOnSidebard(menuName: string) {
+        const locator = await this._page
+            .locator(
+                `//div[contains(@class,'sidebar-items')]//p[contains(@class,'sidebar-item-title')]`
+            )
+            .filter({
+                hasText: menuName,
+            });
+
+        expect(
+            locator.locator(
+                `//ancestor::div[contains(@class,'sidebar-item mt-3')]`
+            )
+        ).toBeTruthy();
+    }
+
+    public async scrollDown() {
+        const locator = await this._page.locator(
             `//div[contains(@class,'sidebar-items')]`
         );
-        const items = await locator.getLocator();
-
-        const itemsText = await items.innerText();
+        await locator.evaluate(() => {
+            window.scrollBy(0, 500);
+        });
     }
 }
--- src/tests/MenuCard/menucard.spec.ts
+++ src/tests/MenuCard/menucard.spec.ts
@@ -2,30 +2,37 @@ import { MenucardHelper } from '@/helpers/Menucardhelper/menucard.helper';
 import { test } from '@playwright/test';
 import { PROCESS_TEST } from '@/fixtures';
 
-test.describe('TFM001', () => {
-    PROCESS_TEST('Check vendor login', async ({ page }) => {
-        const menucard = new MenucardHelper(page);
-        await menucard.init();
-        await menucard.checkPageTitle('AP Dashboard');
-    });
-    PROCESS_TEST('Check Menucard', async ({ page }) => {
-        const menucard = new MenucardHelper(page);
-        await menucard.init();
-        await menucard.openAndCloseMenuCard();
-    });
-
-    PROCESS_TEST('Close Menucard', async ({ page }) => {
-        const menucard = new MenucardHelper(page);
-        await menucard.init();
-        await menucard.openAndCloseMenuCard();
-        await page.waitForTimeout(1000);
-
-        await menucard.openAndCloseMenuCard();
-    });
-    PROCESS_TEST('get SideBar Items', async ({ page }) => {
+test.describe('Menu Crad', () => {
+    PROCESS_TEST('TFM001 - Checking Menu Card', async ({ page }) => {
         const menucard = new MenucardHelper(page);
         await menucard.init();
 
-        await menucard.getSideBarItems();
+        PROCESS_TEST.step('Check Page Title', async () => {
+            await menucard.checkPageTitle('AP Dashboard');
+
+            await page.waitForTimeout(1000);
+        });
+
+        PROCESS_TEST.step('Check SideBar Items', async () => {
+            await menucard.checkSideBarItems();
+        });
+        PROCESS_TEST.step('Check Space in submeny', async () => {
+            await menucard.checkSpaceInMenu('Parties');
+        });
+
+        PROCESS_TEST.step('Check Space on sidebar-item', async () => {
+            await menucard.checkSpaceInMenu('Work Flows');
+        });
+
+        PROCESS_TEST.step('Check SubMenu Items', async () => {
+            await menucard.checkSubMenuItems('Dashboard');
+        });
+
+        PROCESS_TEST.step('Click on Sidebar menu', async () => {
+            await menucard.clickOnSideBarMenu('Dashboard');
+        });
+        PROCESS_TEST.step('Scroll the menu', async () => {
+            await menucard.scrollDown();
+        });
     });
 });
